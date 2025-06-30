2333754cfc141d5ccfc79b9a7b0a7db192befbce
Pushpraj Singh <128183077+pushp2211@users.noreply.github.com>
2025-06-30 07:20:45 +0000

    d3 practicing js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,24 @@
 "use strict" //treat all code as newer version
 
-alert(3 + 3)
+// alert(3 + 3)  //we are using node.js not browser
+
+console.log(3+3)   //code readability should be high
+console.log("pushp")
+
+
+//datatypes
+
+let name="Pushp" //string
+let age=21 //number
+let isLoggedin=false //bool
+
+//number==> 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value...gets output object in typeof
+//undefined =>value not assigned
+//symbol =>unique
+
+//object
+console.log(typeof("pushp"))
---
+++ 01_basics/03_conversionOpe.js
@@ -0,0 +1,39 @@
+let score="33ab"
+
+console.log(typeof(score));
+console.log(typeof(score));
+
+let valueInNNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNNumber);
+
+//"33"=> 33
+//"33abc"=>NaN...type is number
+//true=>1
+
+let isLoggedIn=1;
+let booleanlogged=Boolean(isLoggedIn)
+console.log(booleanlogged);
+
+//1=>true ;0=>false
+//""=>false ;"nonempty"=>true
+
+//*****************operations**********************
+
+let value=3
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);     //2 power 3
+console.log(5%3);
+
+let str1="hii"
+let str2=" pushpraj"
+str3=str1+str2; //appending string directly
+console.log(str3);
+
+
+
