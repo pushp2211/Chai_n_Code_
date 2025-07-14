e9ffe8799cf87daccc40ecfbb1ce9f01d9a73638
Pushpraj Singh <128183077+pushp2211@users.noreply.github.com>
2025-07-14 11:31:39 +0000

    JS D9

--- 05_iterations/04_loops_map.js
+++ 05_iterations/04_loops_map.js
@@ -58,4 +58,4 @@ const userbooks=books.filter( (bk) => bk.genre === "Fiction" && bk.publish < "19
 console.log(userbooks); // [{ title: 'The Great Gatsby', genre: 'Fiction
 //', publish: '1925' }]
 
-// The filter method is used to create a new array with all elements that pass the test implemented by the provided function.
+// The filter method is used to create a new array with all elements that pass the test implemented by the provided function. 
